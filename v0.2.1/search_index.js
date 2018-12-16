00f1743b419bf18ddfff0f61a311230a0c5eb4d8
autodocs <autodocs>
2018-12-16 17:19:27 +0000

    build based on ad72f9b

---
+++ v0.2.1/search_index.js
@@ -0,0 +1,11 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Index",
+    "title": "Index",
+    "category": "page",
+    "text": "Data structure for elements in products of projective spaces.PVector\ndata\ndims\nembed\ndimension_indices\ndimension_indices_homvars\nhomvars\naffine_chart\naffine_chart!\nnorm_affine_chart\nnorm\nnormalize!\nnormalize\ndot\nrmul!\nfubini_study"
+},
+
+]}
